88c285d428ce651da8f71e68a36d31bf1a57ed40
sbx_user1051 <sbx_user1051@169.254.27.149>
2024-10-07 13:58:17 +0000

    Mon Oct 07 13:58:17 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,11 +1,17 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
+def calculate_free_item_count(sku_count, sku, free_item_sku):
+    free_items = sku_count[sku] // 2
+    sku_count[free_item_sku] -= free_items
+
+
 def checkout(skus):
-    item_prices = {"A": 50, "B": 30, "C": 20, "D": 15, "E": 40}
+    item_prices = {"A": 50, "B": 30, "C": 20, "D": 15, "E": 40, "F": 10}
     special_offers = {
         "A": [{"quantity": 5, "price": 200}, {"quantity": 3, "price": 130}],
         "B": [{"quantity": 2, "price": 45}],
-        "E": [{"quantity": 2, "free_item": "B"}]
+        "E": [{"quantity": 2, "free_item": "B"}],
+        "F": [{"quantity": 2, "free_item": "F"}],
     }
 
     if any(char not in item_prices for char in skus):
@@ -15,14 +21,12 @@ def checkout(skus):
     for sku in skus:
         if sku in item_prices:
             sku_count[sku] = sku_count.get(sku, 0) + 1
-    
+
     if "E" in sku_count and "B" in sku_count:
-        free_items = sku_count["E"] // 2
-        sku_count["B"] -= free_items
+        calculate_free_item_count(sku_count, "E", "B")
 
     if "F" in sku_count and sku_count["F"] >= 3:
-        free_items = sku_count["F"] // 2
-        sku_count["F"] -= free_items
+        calculate_free_item_count(sku_count, "F", "F")
 
     total_checkout = 0
     for sku, count in sku_count.items():
@@ -34,7 +38,7 @@ def checkout(skus):
                     offer_count = count // quantity
                     total_checkout += offer_count * price
                     count = count % quantity
-        
+
         total_checkout += count * item_prices[sku]
 
     return total_checkout
