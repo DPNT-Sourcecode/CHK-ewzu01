67aba8544b0ab8d1061dc2f2e47d0f2eef39b762
sbx_user1051 <sbx_user1051@169.254.27.149>
2024-10-07 13:38:26 +0000

    Mon Oct 07 13:38:26 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -5,7 +5,7 @@ def checkout(skus):
     special_offers = {
         "A": [{"quantity": 5, "price": 200}, {"quantity": 3, "price": 130}],
         "B": [{"quantity": 2, "price": 45}],
-        "E": [{"quantity": 2, "free_item": "B"}],
+        "E": [{"quantity": 2, "free_item": "B"}]
     }
 
     if any(char not in item_prices for char in skus):
@@ -15,8 +15,7 @@ def checkout(skus):
     for sku in skus:
         if sku in item_prices:
             sku_count[sku] = sku_count.get(sku, 0) + 1
-
-
+    
     if "E" in sku_count and "B" in sku_count:
         free_items = sku_count["E"] // 2
         sku_count["B"] -= free_items
@@ -25,24 +24,14 @@ def checkout(skus):
     for sku, count in sku_count.items():
         if sku in special_offers and count > 0:
             for special_offer in special_offers[sku]:
-                if count == 0:
-                    continue
+                quantity = special_offer["quantity"]
                 if "price" in special_offer:
                     price = special_offer["price"]
-                    quantity = special_offer["quantity"]
-                    if count % quantity == 0:
-                        total_checkout += (count / quantity) * price
-                        count = 0
-                    elif count < quantity:
-                        total_checkout += count * item_prices[sku]
-                        count = 0
-                    else:
-                        sku_groups = count // quantity
-                        total_checkout += sku_groups * price
-                        count = count - (sku_groups * quantity)
-                else:
-                    total_checkout += count * item_prices[sku]
-        else:
-            total_checkout += count * item_prices[sku]
+                    offer_count = count // quantity
+                    total_checkout += offer_count * price
+                    count = count % quantity
+        
+        total_checkout += count * item_prices[sku]
 
     return total_checkout
+
