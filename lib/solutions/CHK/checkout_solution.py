4a1bc39b89ebf6439ab7d7a2d609b1393bb7b1e5
sbx_user1051 <sbx_user1051@169.254.27.149>
2024-10-07 14:27:06 +0000

    Mon Oct 07 14:27:06 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,49 +6,6 @@ def apply_free_item_offer(sku_count, sku, free_item_sku, quantity):
 
 
 def checkout(skus):
-    item_prices = {
-        "A": 50,
-        "B": 30,
-        "C": 20,
-        "D": 15,
-        "E": 40,
-        "F": 10,
-        "G": 20,
-        "H": 10,
-        "I": 35,
-        "J": 60,
-        "K": 80,
-        "L": 90,
-        "M": 15,
-        "N": 40,
-        "O": 10,
-        "P": 50,
-        "Q": 30,
-        "R": 50,
-        "S": 30,
-        "T": 20,
-        "U": 40,
-        "V": 50,
-        "W": 20,
-        "X": 90,
-        "Y": 10,
-        "Z": 50,
-    }
-    special_offers = {
-        "A": [{"quantity": 5, "price": 200}, {"quantity": 3, "price": 130}],
-        "B": [{"quantity": 2, "price": 45}],
-        "E": [{"quantity": 2, "free_item": "B"}],
-        "F": [{"quantity": 3, "free_item": "F"}],
-        "H": [{"quantity": 10, "price": 80}, {"quantity": 5, "price": 45}],
-        "K": [{"quantity": 2, "price": 150}],
-        "N": [{"quantity": 3, "free_item": "M"}],
-        "P": [{"quantity": 5, "price": 200}],
-        "Q": [{"quantity": 3, "price": 80}],
-        "R": [{"quantity": 3, "free_item": "Q"}],
-        "U": [{"quantity": 3, "free_item": "U"}],
-        "V": [{"quantity": 3, "price": 130}, {"quantity": 2, "price": 90}],
-    }
-
     if any(char not in item_prices for char in skus):
         return -1
 
@@ -62,6 +19,15 @@ def checkout(skus):
 
     if "F" in sku_count and sku_count["F"] >= 3:
         apply_free_item_offer(sku_count, "F", "F", 3)
+
+    if "N" in sku_count and "M" in sku_count:
+        apply_free_item_offer(sku_count, "N", "M", 3)
+        
+    if "R" in sku_count and "Q" in sku_count:
+        apply_free_item_offer(sku_count, "R", "Q", 3)
+        
+    if "U" in sku_count:
+        apply_free_item_offer(sku_count, "U", "U", 3)
 
     total_checkout = 0
     for sku, count in sku_count.items():
@@ -77,5 +43,6 @@ def checkout(skus):
         total_checkout += count * item_prices[sku]
 
     return total_checkout
+
 
 
