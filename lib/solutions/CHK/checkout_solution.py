d278b7b3444002ec36924af95576f141a61d8aa0
sbx_user1051 <sbx_user1051@169.254.27.149>
2024-10-07 11:32:55 +0000

    Mon Oct 07 11:32:55 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,6 +4,12 @@
 # skus = unicode string
 def checkout(skus):
     item_prices = {"A": 50, "B": 30, "C": 20, "D": 15}
-    special_offers = {"A": }
+    special_offers = {"A": {"quantity": 3, "price": 130}, "B": {"quantity": 2, "price": 45}}
+
+    if any(char not in skus for char in skus):
+        return -1
+    
+
+
 
 
