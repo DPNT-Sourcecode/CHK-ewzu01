61e107e4abefd8480705088d95b3ebcfd8e14f54
sbx_user1051 <sbx_user1051@169.254.27.149>
2024-10-07 18:14:24 +0000

    Mon Oct 07 18:14:24 UTC 2024

--- lib/solutions/CHK/helpers.py
+++ lib/solutions/CHK/helpers.py
@@ -42,15 +42,12 @@ def apply_group_discount(
     discounted_price = discount_groups_count * group_price
 
     items_to_remove_count = discount_groups_count * group_size
-    for item in group_items:
+    for item in sku_count:
         if items_to_remove_count <= 0:
-            break
-        if item in sku_count:
-            if sku_count[item] <= items_to_remove_count:
-                items_to_remove_count -= sku_count[item]
-                sku_count[item] = 0
-            else:
-                sku_count[item] -= items_to_remove_count
-                items_to_remove_count = 0
+                break
+        while sku_count[item] > 0:
+            sku_count[item] -= 1
+            items_to_remove_count -= 1
 
     return discounted_price
+
