7c17067bd4223746dfeb198919fde757fcf53ea8
sbx_user1051 <sbx_user1051@169.254.27.149>
2024-10-07 13:02:26 +0000

    Mon Oct 07 13:02:26 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,10 +1,11 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    item_prices = {"A": 50, "B": 30, "C": 20, "D": 15}
+    item_prices = {"A": 50, "B": 30, "C": 20, "D": 15, "E": 40}
     special_offers = {
-        "A": {"quantity": 3, "price": 130},
-        "B": {"quantity": 2, "price": 45},
+        "A": [{"quantity": 5, "price": 200}, {"quantity": 3, "price": 130}],
+        "B": [{"quantity": 2, "price": 45}],
+        "E": [{"quantity": 2, "free_item": "B"}],
     }
 
     if any(char not in item_prices for char in skus):
@@ -15,21 +16,32 @@ def checkout(skus):
         if sku in item_prices:
             sku_count[sku] = sku_count.get(sku, 0) + 1
 
+
+    if "E" in sku_count and "B" in sku_count:
+        free_items = sku_count["E"] // 2
+        sku_count["B"] -= free_items
+
     total_checkout = 0
     for sku, count in sku_count.items():
-        if sku in special_offers:
-            price = special_offers[sku]["price"]
-            quantity = special_offers[sku]["quantity"]
-            if count == quantity:
-                total_checkout += price
-            elif count % quantity == 0:
-                total_checkout += (count / quantity) * price
-            elif count < quantity:
-                total_checkout += count * item_prices[sku]
-            else:
-                sku_groups = count // quantity
-                remaining_skus = count - (sku_groups * quantity)
-                total_checkout += (sku_groups * price) + (remaining_skus * item_prices[sku])
+        if sku in special_offers and count > 0:
+            for special_offer in special_offers[sku]:
+                if count == 0:
+                    continue
+                if "price" in special_offer:
+                    price = special_offer["price"]
+                    quantity = special_offer["quantity"]
+                    if count % quantity == 0:
+                        total_checkout += (count / quantity) * price
+                        count = 0
+                    elif count < quantity:
+                        total_checkout += count * item_prices[sku]
+                        count = 0
+                    else:
+                        sku_groups = count // quantity
+                        total_checkout += sku_groups * price
+                        count = count - (sku_groups * quantity)
+                else:
+                    total_checkout += count * item_prices[sku]
         else:
             total_checkout += count * item_prices[sku]
 
