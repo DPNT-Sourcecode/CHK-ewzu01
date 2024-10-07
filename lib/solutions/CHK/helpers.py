0f78e8841b347612a9a4d876e4a9650e4fd1075f
sbx_user1051 <sbx_user1051@169.254.27.149>
2024-10-07 15:24:24 +0000

    Mon Oct 07 15:24:24 UTC 2024

--- lib/solutions/CHK/helpers.py
+++ lib/solutions/CHK/helpers.py
@@ -1,7 +1,5 @@
-def apply_free_item_offer(sku_count, sku, free_item_sku, quantity, basket_condition=None):
-    if sku_count[sku] == quantity and not basket_condition:
-        free_items = 0
-    else:
-        free_items = sku_count[sku] // quantity
+def apply_free_item_offer(sku_count, sku, free_item_sku, quantity):
+    free_items = sku_count[sku] // quantity
     sku_count[free_item_sku] -= free_items
+
 
